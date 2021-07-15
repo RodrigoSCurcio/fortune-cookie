99b05757795a6ea5b0d3fbded1762b708e920075
RodrigoSCurcio <curcio.the.rod@gmail.com>
2021-07-15 11:57:30 -0300

    React Js do zero ao avançado na pratica

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: ''
+    };
+
+    this.breakCookie = this.breakCookie.bind(this);
+
+    this.texts = [
+      'A vida trará coisas boas se tiver paciência.',
+      'Demonstre amor e alegria em todas as oportunidades e verá que a paz nasce dentro de si.',
+      'Não compense na ira o que lhe falta na razão.',
+      'Defeitos e virtudes são apenas dois lados da mesma moeda.',
+      'Não há que ser forte. Há que ser flexível.',
+      'Todos os dias organiza os seus cabelos, por que não faz o mesmo com o coração?' ,
+      'Há três coisas que jamais voltam; a flecha lançada, a palavra dita e a oportunidade perdida.',
+      'A juventude não é uma época da vida, é um estado de espírito.',
+      'Podemos escolher o que semear, mas somos obrigados a colher o que plantamos.',
+    ]
+  }
+
+  breakCookie() {
+    let state = this.state;
+    let randomNumber = Math.floor(Math.random() * this.texts.length);
+    state.text = '"' + this.texts[randomNumber] + '"';
+    this.setState(state);
+  }
+
+
+  render() {
+    return (
+      <div className="container">
+        <img src='/assets/biscoito.png' />
+        <Botao name="Abrir biscoito" actionBtn={this.breakCookie} />
+        <h3>{this.state.text}</h3>
+      </div>
+    );
+  }
+}
+
+class Botao extends Component {
+  render() {
+    return (
+      <div>
+        <button onClick={this.props.actionBtn}>
+          {this.props.name}
+        </button>
+      </div>
+    )
+  }
 }
 
 export default App;
